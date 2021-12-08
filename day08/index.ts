b7322ff9d49f7ebdc340394660ad673a13837cd6
Ben Lee <benlee.thatsme@gmail.com>
2021-12-08 10:21:32 +0000

    tidy up day 8

--- day08/index.ts
+++ day08/index.ts
@@ -40,58 +40,55 @@ export function part2(input: string): any {
     numberMap[seven] = 7;
     numberMap[eight] = 8;
 
-    // 3 is the signal with length of 5 which if you take whats in 1 away you are left with 3 length
+    // three is the signal with length of 5 which if you take whats in 1 away you are left with 3 length
     const three =
-      joined
-        .filter((signal) => signal.length === 5)
-        .find((signal) => diffSignal(one, signal).length === 3) ?? "";
+      joined.find(
+        (signal) => signal.length === 5 && diffSignal(one, signal).length === 3
+      ) ?? "";
 
     numberMap[three] = 3;
 
     // b is the the letter that are different between 1 and 4 not in 3
-    const b = diffSignal(one, four)
-      .split("")
-      .find((letter) => !three.includes(letter)) as string;
-
     // d is the the letter that are different between 1 and 4 and is in 3
-    const d = diffSignal(one, four)
+    const [b, d] = diffSignal(one, four)
       .split("")
-      .find((letter) => letter !== b) as string;
+      .sort((a) => (three.includes(a) ? 1 : -1));
 
-    // two is the 5 length signal that does not contain b and is not three
-    const two = joined
-      .filter((signal) => signal.length === 5)
-      .find((signal) => !signal.includes(b) && signal !== three) as string;
+    // two is the 5 length signal that does not contain b and is not equal to three
+    const two = joined.find(
+      (signal) => signal.length === 5 && !signal.includes(b) && signal !== three
+    ) as string;
 
     numberMap[two] = 2;
 
-    // five is the 5 length signal that does contain b and is not 3
-    const five = joined
-      .filter((signal) => signal.length === 5)
-      .find((signal) => signal.includes(b) && signal !== three) as string;
+    // five is the 5 length signal that does contain b and is not equal to three
+    const five = joined.find(
+      (signal) => signal.length === 5 && signal.includes(b) && signal !== three
+    ) as string;
 
     numberMap[five] = 5;
 
     // zero is the 6 length signal that does not contain d
-    const zero = joined
-      .filter((signal) => signal.length === 6)
-      .find((signal) => !signal.includes(d)) as string;
+    const zero = joined.find(
+      (signal) => signal.length === 6 && !signal.includes(d)
+    ) as string;
 
     numberMap[zero] = 0;
 
-    // 6 is the 6 length signal not equal to zero with 5 left after diff 1
-    const six = joined
-      .filter((signal) => signal.length === 6)
-      .find(
-        (signal) => signal !== zero && diffSignal(signal, one).length === 5
-      ) as string;
+    // six is the 6 length signal not equal to zero with 5 left after diff 1
+    const six = joined.find(
+      (signal) =>
+        signal.length === 6 &&
+        signal !== zero &&
+        diffSignal(signal, one).length === 5
+    ) as string;
 
     numberMap[six] = 6;
 
-    // 9 has the length of 6 and is not 0 or 6
-    const nine = joined
-      .filter((signal) => signal.length === 6)
-      .find((signal) => signal !== zero && signal !== six) as string;
+    // 9 has the length of 6 and is not equal to zero or six
+    const nine = joined.find(
+      (signal) => signal.length === 6 && signal !== zero && signal !== six
+    ) as string;
 
     numberMap[nine] = 9;
 
